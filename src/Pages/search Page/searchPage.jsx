c4d0bb643cc3f117e1a57ab50b024ccbeed9636b
Rajesh270712 <rajeshbagul2001@gmail.com>
2022-06-28 18:35:21 +0530

    filter added

--- src/Pages/search Page/searchPage.jsx
+++ src/Pages/search Page/searchPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getInputData, sortByCategory, sortByJobType, sortByLocation } from '../../Redux/search Page/action';
+import { getInputData, sortByCategory, sortByJobType, sortByLocation, sortByTechStack } from '../../Redux/search Page/action';
 import { Box, Heading, Text, HStack, Grid,GridItem, VStack } from '@chakra-ui/react';
 import moment from 'moment';
 import FilterData from './filterData';
@@ -34,25 +34,32 @@ export const SearchPage = () => {
     dispatch(sortByLocation(location))
   
    }
-  function handleJobTypeChange(location){
+  function handleJobTypeChange(JobType){
     dispatch(getInputData(searchResult));
-    dispatch(sortByJobType(location))
+    dispatch(sortByJobType(JobType))
   
    }
-  function handleCategoryChange(location){
+  function handleCategoryChange(Category){
     dispatch(getInputData(searchResult));
-    dispatch(sortByCategory(location))
+    dispatch(sortByCategory(Category))
+  
+   }
+  function handleTechStackChange(TechStack){
+    dispatch(getInputData(searchResult));
+    dispatch(sortByTechStack(TechStack))
   
    }
 
   return (
     <Box className="searchResultContainer">
+
       <div>
-       <FilterData handleCategoryChange={handleCategoryChange} handleLocationChange={handleLocationChange} handleJobTypeChange={handleJobTypeChange} />
+       <FilterData handleTechStackChange={handleTechStackChange} handleCategoryChange={handleCategoryChange} handleLocationChange={handleLocationChange} handleJobTypeChange={handleJobTypeChange} />
       </div>
       
       <GridItem>
-      {searchData.map(job => (
+        <Text ml={2} className="searchPara"  > 1 - 20 of 101 {searchInput} Jobs</Text>
+      {searchData.slice(0,15).map(job => (
         <div key={job.id} className="jobBox">
           <Heading as="h1">{job.title}</Heading>
           <HStack key={job.id} className="companyName" spacing="8px">
@@ -72,18 +79,22 @@ export const SearchPage = () => {
             spacing="20px"
             
           >
-            {job.tags.map((tag, index) =>
-              index < 5 ? (
-                <Text>{tag.charAt(0).toUpperCase() + tag.slice(1)} </Text>
-              ) : index === 5 ? (
-                <Text>{tag.charAt(0).toUpperCase() + tag.slice(1)} </Text>
-              ) : null
+            {job.tags.slice(0,7).map((tag, index) =>
+              
+                <Text>·  {tag.charAt(0).toUpperCase() + tag.slice(1)} </Text>
+              
             )}
           </HStack>
           
         </div>
       ))}
       </GridItem>
+      <GridItem marginLeft="16%" >
+        <Box >
+
+        <h1>add</h1>
+        </Box>
+      </GridItem >
     </Box>
   );
 };
