73e917869d760f925af30d61cbe6e068ec20606f
Rajesh270712 <rajeshbagul2001@gmail.com>
2022-06-30 16:07:46 +0530

    loading added

--- src/Redux/search Page/reducer.jsx
+++ src/Redux/search Page/reducer.jsx
@@ -1,15 +1,18 @@
-import {  GET_INPUT_DATA, SORT_BY_CATEGORY, SORT_BY_JOB_TYPE, SORT_BY_LOCATION, SORT_BY_TECH_STACK } from "./action"
+import {  GET_INPUT_DATA, IS_ERROR, IS_LOADING, SORT_BY_CATEGORY, SORT_BY_JOB_TYPE, SORT_BY_LOCATION, SORT_BY_TECH_STACK } from "./action"
 
 let initState ={
     searchData:[],
-    jobById:{}
+    loading:false,
+    error:false
 }
 
 export const searchReducer =(state=initState, {type,payload}) => {
     switch (type){
         case GET_INPUT_DATA:{
             return{
-                searchData:[...payload]
+                searchData:[...payload],
+                loading:false,
+                error:false
             }
         }
         case SORT_BY_LOCATION:{
@@ -17,7 +20,9 @@ export const searchReducer =(state=initState, {type,payload}) => {
                 ...state,
                 searchData : state.searchData.filter((job)=>(
                     job.candidate_required_location===payload ? [job] : null
-                ))
+                )),
+                loading:false,
+                error:false
             }
         }
         case SORT_BY_JOB_TYPE:{
@@ -25,7 +30,9 @@ export const searchReducer =(state=initState, {type,payload}) => {
                 ...state,
                 searchData : state.searchData.filter((job)=>(
                     job.job_type===payload ? [job] : null
-                ))
+                )),
+                loading:false,
+                error:false
             }
         }
         case SORT_BY_CATEGORY:{
@@ -33,7 +40,9 @@ export const searchReducer =(state=initState, {type,payload}) => {
                 ...state,
                 searchData : state.searchData.filter((job)=>(
                     job.category===payload ? [job] : null
-                ))
+                )),
+                loading:false,
+                error:false
             }
         }
         case SORT_BY_TECH_STACK:{
@@ -44,7 +53,23 @@ export const searchReducer =(state=initState, {type,payload}) => {
                         jobTag===payload ? [job] : null
                     ))
                     // job.category===payload ? [job] : null
-                ))
+                )),
+                loading:false,
+                error:false
+            }
+        }
+        case IS_LOADING:{
+            return{
+                ...state,
+                loading:payload,
+                error:false
+            }
+        }
+        case IS_ERROR:{
+            return{
+                ...state,
+                loading:false,
+                error:true
             }
         }
         default:
